1ffb199f126d6fcaaa0eb1745941af7f563bf4af
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-17 17:54:55 +0100

    update Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,28 +1,49 @@
 import { useState } from "react";
 
 export default function Accordion({ data }) {
-  const [curOpen, setisOpen] = useState(1);
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el) => (
         <AccordionItem
           curOpen={curOpen}
-          onOpen={setisOpen}
+          onOpen={setCurOpen}
           key={el.num}
           title={el.title}
-          text={el.text}
           num={el.num}
-        />
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        key="test 1"
+        title="What is React?"
+        num={22}
+      >
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          nihil!
+        </p>
+        <ul>
+          <li>Lorem, ipsum.</li>
+          <li>Lorem, ipsum.</li>
+          <li>Lorem, ipsum.</li>
+          <li>Lorem, ipsum.</li>
+          <li>Lorem, ipsum.</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text, curOpen, onOpen }) {
+function AccordionItem({ num, title, children, curOpen, onOpen }) {
   const isOpen = num === curOpen;
 
   const handleIsOpen = () => {
-    setisOpen(!isOpen);
+    onOpen(isOpen ? null : num);
   };
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleIsOpen}>
@@ -30,7 +51,7 @@ function AccordionItem({ num, title, text, curOpen, onOpen }) {
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
